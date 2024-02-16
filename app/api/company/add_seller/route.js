61c71eae2b52a317e182e568cbcd6178f294cf54
vengateshwaran1 <kvengateshwaran1@gmail.com>
2024-02-16 23:59:28 +0530

    adding sellers

---
+++ app/api/company/add_seller/route.js
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+import pool from  "../../../../utils/db";
+const bcrypt = require("bcrypt");
+
+export async function POST(req, { params }) {
+    try {
+        const { s_type, s_name, s_mail} = req.body;
+
+        const db = await pool.getConnection();
+        const result = await db.query('INSERT INTO seller (s_type, s_name, s_mail) VALUES (?, ?, ?)', [s_type, s_name, s_mail]);
+        db.release();
+
+        console.log('Seller added:', result);
+        return new NextResponse("Product added successfully", { status: 200 });
+    } 
+    catch (error) {
+        console.error('Error adding seller:', error);
+        return new NextResponse("Error adding seller", { status: 500 });
+    }
+};
